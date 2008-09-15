b235205a2e49c35c151394be407001929857b350
mia <mia@68-246-109-90.area3.spcsdns.net>
2008-09-15 10:33:11 -0400

    added tag_cloud method applicaiton helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  include TagsHelper
+  
+  def tag_cloud
+    @tags = Stage.tag_counts
+  end
   
   def nice_flash(text)
     render(:partial => 'layouts/flash', :locals => {:text => text})
