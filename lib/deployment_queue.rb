3d3fca21d84b2ca45663cacdc948548dfdf87c18
mia <mia@cus5hpnm81.corp.chartoneinc.com>
2008-09-16 14:04:24 -0400

    DeplymentQueue Module Added

---
+++ lib/deployment_queue.rb
@@ -0,0 +1,46 @@
+module DeploymentQueue
+  
+  class Manager
+
+    require 'memcache'
+    attr_accessor :starling
+
+    def initialize(address, port)
+      @starling = MemCache.new("#{address}:#{port}") 
+    end
+
+    def push(item)
+      @starling.set('my_queue', item)
+    end
+
+  end
+  
+  class Deployer
+    require 'memcache'
+    require 'deployment'
+    attr_accessor :starling
+
+    def initialize(address, port)
+      @starling = MemCache.new("#{address}:#{port}") 
+      start
+    end
+    
+    def start
+      until 1 == 0
+        did = @starling.get('my_queue')
+        if did != nil
+          @deployment = Deployment.find(did)
+          @deployment.deploy_in_background!
+          until @deployment.completed_at != nil
+            puts "Waiting to complete #{did}"
+            @deployment.reload
+            sleep(1)
+          end
+        end
+        puts "No Work"
+        sleep(1)
+      end
+    end
+  end
+  
+end
